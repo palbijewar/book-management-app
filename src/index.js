33a7745bae03492c283e9fd7f53d54a637a6a5e2
Pal Bijewar <pal.bijewar@gmail.com>
2023-06-17 13:11:50 +0530

    starter template created for the app

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import users from './routes/usersRoute.js';
+import books from './routes/booksRoute.js';
+import reviews from './routes/reviewsRoute.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser:true
+    }).then(()=>{console.log('Mongoose connected!')}).catch((error)=>console.log(error));
+    
+app.get('/',(req,res)=>{
+        res.status(200).json({status : true, message : "API is Live"})
+      })
+        
+app.use('/',users);
+app.use('/',books);
+app.use('/',reviews);
+
+
+
+app.listen(process.env.PORT, ()=>{
+            console.log(`server running at PORT : ${process.env.PORT}`)
+        });
+
+
+
+
+
